c88f83ab919c0243becade613cb4cdf9bf3d4453
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-10 16:30:33 -0500

    Initial Commit

---
+++ DemoCoreData20230210/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DemoCoreData20230210
+//
+//  Created by Xavier on 2/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoCoreData20230210/DemoCoreData20230210App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCoreData20230210App.swift
+//  DemoCoreData20230210
+//
+//  Created by Xavier on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoCoreData20230210App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
